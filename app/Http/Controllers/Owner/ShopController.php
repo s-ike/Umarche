bb0f297d79ba673cd5c399907fc851b6fe6df862
s-ike <shunt_i@yahoo.co.jp>
2022-02-24 18:41:35 +0900

    add shop edit & update

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,8 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
-use InterventionImage;
 use App\Http\Requests\UploadImageRequest;
 use App\Services\ImageService;
 
@@ -71,12 +69,28 @@ class ShopController extends Controller
      */
     public function update(UploadImageRequest $request, $id)
     {
+        $request->validate([
+            'name' => ['required', 'string', 'max:50'],
+            'information' => ['required', 'string', 'max:1000'],
+            'is_selling' => ['required'],
+        ]);
+
         $imageFile = $request->image;
         if (!is_null($imageFile) && $imageFile->isValid()) {
             $fileNameToStore = ImageService::upload($imageFile, 'shops');
         }
 
-        return redirect()
-            ->route('owner.shops.index');
+        $shop = Shop::findOrFail($id);
+        $shop->name = $request->name;
+        $shop->information = $request->information;
+        $shop->is_selling = $request->is_selling;
+        if (!is_null($imageFile) && $imageFile->isValid()) {
+            $shop->filename = $fileNameToStore;
         }
+        $shop->save();
+
+        return redirect()
+            ->route('owner.shops.index')
+            ->with(['message' => '店舗情報を更新しました。', 'status' => 'info']);
+    }
 }
