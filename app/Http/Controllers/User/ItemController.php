6005c03f0f140170fae7371cb9dac5027f7157b7
s-ike <shunt_i@yahoo.co.jp>
2022-02-27 10:23:51 +0900

    add products query

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -4,13 +4,41 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
     public function index()
     {
-        $products = Product::all();
+        $stocks = DB::table('t_stocks')
+            ->select('product_id', DB::raw('sum(quantity) AS quantity'))
+            ->groupBy('product_id')
+            ->having('quantity', '>', 1);
+
+        $products = DB::table('products')
+            ->joinSub($stocks, 'stock', function ($join) {
+                $join->on('products.id', '=', 'stock.product_id');
+            })
+            ->join('shops', 'products.shop_id', '=', 'shops.id')
+            ->join('secondary_categories', 'products.secondary_category_id', '=', 'secondary_categories.id')
+            ->join('images AS image1', 'products.image1', '=', 'image1.id')
+            ->join('images AS image2', 'products.image2', '=', 'image2.id')
+            ->join('images AS image3', 'products.image3', '=', 'image3.id')
+            ->join('images AS image4', 'products.image4', '=', 'image4.id')
+            ->where('shops.is_selling', true)
+            ->where('products.is_selling', true)
+            ->select(
+                'products.id AS id',
+                'products.name AS name',
+                'products.price',
+                'products.sort_order AS sort_order',
+                'products.information AS information',
+                'secondary_categories.name AS category',
+                'image1.filename AS filename'
+            )
+            ->get();
+
+        // dd($stocks, $products);
         return view('user.index', compact('products'));
     }
 }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,14 +10,13 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex flex-wrap">
-
                         @foreach ($products as $product)
                             <div class="w-1/4 p-2 md:p-4">
                                 <a href="">
                                     <div class="border rounded-md p-2 md:p-4">
-                                        <x-thumbnail filename="{{ $product->imageFirst->filename ?? '' }}" type="products" />
+                                        <x-thumbnail filename="{{ $product->filename ?? '' }}" type="products" />
                                         <div class="mt-4">
-                                            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->category->name }}</h3>
+                                            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->category }}</h3>
                                             <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}</h2>
                                             <p class="mt-1">{{ number_format($product->price) }}<span class="text-sm text-gray-700">円（税込）</span></p>
                                         </div>
@@ -25,7 +24,6 @@
                                 </a>
                             </div>
                         @endforeach
-
                     </div>
                 </div>
             </div>
