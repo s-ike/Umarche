4e4389967cf03d00846a5b4947504092335faae4
s-ike <shunt_i@yahoo.co.jp>
2022-03-01 11:58:48 +0900

    add processing when canceling Stripe

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,9 @@ Route::prefix('cart')->middleware(['auth:users'])->group(function () {
 
     Route::get('success', [CartController::class, 'success'])
         ->name('cart.success');
+
+    Route::get('cancel', [CartController::class, 'cancel'])
+        ->name('cart.cancel');
 });
 
 // Route::get('/dashboard', function () {
