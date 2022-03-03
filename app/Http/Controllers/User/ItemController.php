8008974d612b892e80f0c04341719d49d8a5f8ad
s-ike <shunt_i@yahoo.co.jp>
2022-03-03 14:31:57 +0900

    update to change peoducts query to local scope

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -18,50 +18,12 @@ class ItemController extends Controller
     public function __construct()
     {
         $this->middleware('auth:users');
-    //     $this->middleware(function ($request, $next) {
-    //         $id = $request->route()->parameter('product');
-    //         if (!is_null($id)) {
-    //             $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
-    //             $productsOwnerId = (int)$productsOwnerId;
-    //             if ($productsOwnerId !== Auth::id()) {
-    //                 abort(404);
-    //             }
-    //         }
-    //         return $next($request);
-    //     });
     }
 
     public function index()
     {
-        $stocks = DB::table('t_stocks')
-            ->select('product_id', DB::raw('sum(quantity) AS quantity'))
-            ->groupBy('product_id')
-            ->having('quantity', '>', 1);
+        $products = Product::availableItems()->get();
 
-        $products = DB::table('products')
-            ->joinSub($stocks, 'stock', function ($join) {
-                $join->on('products.id', '=', 'stock.product_id');
-            })
-            ->join('shops', 'products.shop_id', '=', 'shops.id')
-            ->join('secondary_categories', 'products.secondary_category_id', '=', 'secondary_categories.id')
-            ->join('images AS image1', 'products.image1', '=', 'image1.id')
-            ->join('images AS image2', 'products.image2', '=', 'image2.id')
-            ->join('images AS image3', 'products.image3', '=', 'image3.id')
-            ->join('images AS image4', 'products.image4', '=', 'image4.id')
-            ->where('shops.is_selling', true)
-            ->where('products.is_selling', true)
-            ->select(
-                'products.id AS id',
-                'products.name AS name',
-                'products.price',
-                'products.sort_order AS sort_order',
-                'products.information AS information',
-                'secondary_categories.name AS category',
-                'image1.filename AS filename'
-            )
-            ->get();
-
-        // dd($stocks, $products);
         return view('user.index', compact('products'));
     }
 
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use App\Models\Shop;
 use App\Models\SecondaryCategory;
 use App\Models\Image;
@@ -73,5 +74,38 @@ class Product extends Model
     {
         return $this->belongsToMany(User::class, 'carts')
             ->withPivot(['id', 'quantity']);
+    }
+
+    /**
+     * 販売中の商品を取得するスコープを設定
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeAvailableItems($query)
+    {
+        $stocks = DB::table('t_stocks')
+            ->select('product_id', DB::raw('sum(quantity) AS quantity'))
+            ->groupBy('product_id')
+            ->having('quantity', '>', 1);
+
+        return $query
+            ->joinSub($stocks, 'stock', function ($join) {
+                $join->on('products.id', '=', 'stock.product_id');
+            })
+            ->join('shops', 'products.shop_id', '=', 'shops.id')
+            ->join('secondary_categories', 'products.secondary_category_id', '=', 'secondary_categories.id')
+            ->join('images AS image1', 'products.image1', '=', 'image1.id')
+            ->where('shops.is_selling', true)
+            ->where('products.is_selling', true)
+            ->select(
+                'products.id AS id',
+                'products.name AS name',
+                'products.price',
+                'products.sort_order AS sort_order',
+                'products.information AS information',
+                'secondary_categories.name AS category',
+                'image1.filename AS filename'
+            );
     }
 }
